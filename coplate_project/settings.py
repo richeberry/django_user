0eeb8ceead40b7d8178637e1da386c2a9c1e51b7
Song Song Y <65588098+richeberry@users.noreply.github.com>
2022-12-02 16:13:53 +0900

    Update settings.py

--- coplate_project/settings.py
+++ coplate_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9idg(vb=l_6m$en0-z(ms$b&y6_i#wk3cclzx8x-teeha=ik%c'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
