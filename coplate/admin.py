d01c579fc9bbd2799dc1d5ec41326d6460ea9521
richeberry <richeberry@gmail.com>
2022-12-04 22:37:33 +0900

    nickname admin page

--- coplate/admin.py
+++ coplate/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
+
 # Register your models here.
 admin.site.register(User, UserAdmin)
+# admin 페이지에는 닉네임 필드가 없으므로 custom field를 만들어서 닉네임 필드를 만듦
+# <class 'django.contrib.auth.admin.UserAdmin'>: (admin.E008) The value of 'fieldsets[5]' must be a list or tuple.
+UserAdmin.fieldsets += ("Custom fields", {"fields": ("nickname",)})
--- coplate/models.py
+++ coplate/models.py
@@ -4,5 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 # User 인스턴스 출력 시에 사용 _ email을 사용할 것이기 때문에 덮어씌움
 class User(AbstractUser):
+    nickname = models.CharField(max_length=15, unique=True, null=True)
+    
     def __str__(self):
         return self.email
